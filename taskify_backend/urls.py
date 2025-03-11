12d56d5fe2cc7d983843822a7ed7b5f2085a6111
Carlos <carloscuba014@gmail.com>
2025-03-11 17:03:45 -0400

    build rest api

--- taskify_backend/urls.py
+++ taskify_backend/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from tasks.views import ProjectViewSet, TaskViewSet
+
+router = DefaultRouter()
+router.register(r'projects', ProjectViewSet)
+router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
