ca571243c00c4a880ed416e0b1fac700ed11c06c
Carlos <carloscuba014@gmail.com>
2025-03-12 12:21:55 -0400

    implement authentication

--- taskify_backend/urls.py
+++ taskify_backend/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from tasks.views import ProjectViewSet, TaskViewSet
 
 router = DefaultRouter()
@@ -25,4 +26,6 @@ router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
