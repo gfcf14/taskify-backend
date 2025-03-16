d89aa54634b75011c234c84c822b3c736b76356b
Carlos <carloscuba014@gmail.com>
2025-03-15 20:33:07 -0400

    enable django admin

--- taskify_backend/urls.py
+++ taskify_backend/urls.py
@@ -25,6 +25,7 @@ router.register(r'projects', ProjectViewSet)
 router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
